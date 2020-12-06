9859e7d3c6f0d59b979c705e33dfc49f9a7c9e81
bregarulai <bregarulai@gmail.com>
2020-12-05 21:22:25 -0500

    added logo and brand name to nav bar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,36 @@
+import { graphql, useStaticQuery } from "gatsby"
 import React from "react"
+import Image from "gatsby-image"
+
+const query = graphql`
+  {
+    file(relativePath: { eq: "logo.png" }) {
+      image: childImageSharp {
+        fluid(maxWidth: 300) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
 
 const Navigation = () => {
+  const {
+    file: {
+      image: { fluid },
+    },
+  } = useStaticQuery(query)
+
   return (
-    <header>
-      <p>navigation</p>
+    <header className="navigation">
+      <div className="u-section-center">
+        <div className="navigation__brand">
+          <div className="navigation__img-box">
+            <Image fluid={fluid} alt="Hairstylist Hair by B logo" />
+          </div>
+          <p>Hair by B</p>
+        </div>
+      </div>
     </header>
   )
 }
