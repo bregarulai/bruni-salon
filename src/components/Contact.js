8adbe998e46da3b39c2e3600a1b0ea3bec31165a
bregarulai <bregarulai@gmail.com>
2020-12-09 17:27:40 -0500

    added contact send button

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,5 @@
 import React from "react"
+import Form from "./Form"
 
 const Contact = () => {
   return (
@@ -8,61 +9,10 @@ const Contact = () => {
           contact
         </h3>
         <div className="contact__form">
-          <form action="" className="form">
-            <div>
-              <h4 className="heading-quaternary u-margin-bottom-medium u-center-text">
-                send a message for an appointment
-              </h4>
-            </div>
-            <div className="form__group">
-              <input
-                id="name"
-                type="text"
-                className="form__input"
-                placeholder="Name"
-                required
-              />
-              <label htmlFor="name" className="form__label">
-                Name
-              </label>
-            </div>
-            <div className="form__group">
-              <input
-                id="cell"
-                type="text"
-                className="form__input"
-                placeholder="Cell"
-                required
-              />
-              <label htmlFor="cell" className="form__label">
-                Cell
-              </label>
-            </div>
-            <div className="form__group">
-              <input
-                id="email"
-                type="email"
-                className="form__input"
-                placeholder="Email"
-                required
-              />
-              <label htmlFor="email" className="form__label">
-                Email
-              </label>
-            </div>
-            <div className="form__group">
-              <textarea
-                id="message"
-                type="textarea"
-                className="form__input"
-                placeholder="Message"
-                required
-              />
-              <label htmlFor="message" className="form__label">
-                Message
-              </label>
-            </div>
-          </form>
+          <Form />
+          <button className="btn btn--primary btn--submit u-center-text">
+            send
+          </button>
         </div>
       </div>
     </section>
