75b53c8f4aa6aaf268f78d4bed08cb921cc0e68f
bregarulai <bregarulai@gmail.com>
2020-12-08 19:00:45 -0500

    added border radius to images

--- src/components/ServiceCard.js
+++ src/components/ServiceCard.js
@@ -2,23 +2,22 @@ import React from "react"
 import Image from "gatsby-image"
 
 const ServiceCard = ({ service }) => {
+  const {
+    title,
+    altText,
+    desc: { description },
+    image: { fluid },
+  } = service
   return (
     <div className="services__card">
       <div className="services__content">
         <h3 className="heading-quaternary u-center-text u-margin-top-medium u-margin-bottom-small">
-          hair color
+          {title}
         </h3>
-        <p className="paragraph u-margin-bottom-medium">
-          I'm baby quinoa edison bulb normcore plaid. Polaroid venmo locavore
-          succulents tbh. Messenger bag pinterest taiyaki twee freegan tousled
-          woke raclette kale chips succulents.
-        </p>
+        <p className="paragraph u-margin-bottom-medium">{description}</p>
       </div>
       <div className="services__img-box">
-        <Image
-          fluid={colorImage}
-          alt="Hair stylist Hair by B service hair color image"
-        />
+        <Image fluid={fluid} alt={altText} className="services__img" />
       </div>
     </div>
   )
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,12 +1,21 @@
 import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
+import ServiceCard from "./ServiceCard"
 
 const query = graphql`
   {
-    serviceColor: file(relativePath: { eq: "service-color.jpg" }) {
-      image: childImageSharp {
-        fluid(maxWidth: 300) {
-          ...GatsbyImageSharpFluid
+    services: allContentfulServiceBruni {
+      nodes {
+        id
+        title
+        altText
+        desc: childContentfulServiceBruniDescriptionTextNode {
+          description
+        }
+        image {
+          fluid {
+            ...GatsbyContentfulFluid
+          }
         }
       }
     }
@@ -15,15 +24,17 @@ const query = graphql`
 
 const Services = () => {
   const {
-    serviceColor: {
-      image: { fluid: colorImage },
-    },
+    services: { nodes },
   } = useStaticQuery(query)
   return (
     <section className="u-section services">
       <div className="u-section-center">
         <h2 className="heading-tertiary u-center-text">services</h2>
-        <div className="services__container"></div>
+        <div className="services__container">
+          {nodes.map(service => {
+            return <ServiceCard key={service.id} service={service} />
+          })}
+        </div>
       </div>
     </section>
   )
