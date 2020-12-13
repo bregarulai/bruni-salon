41ce55ae7b6eda5aa06342f27c5a1f5715c4772b
bregarulai <bregarulai@gmail.com>
2020-12-13 13:11:29 -0500

    adjusted grid on hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -35,27 +35,26 @@ const Hero = () => {
     <section id="home" className="u-section hero">
       <div className="u-section-center">
         <div className="hero__container">
-          <div className="hero__content">
-            <h1 className="heading-primary">
-              <span className="heading-primary--main">hair stylist</span>
-              <span className="heading-primary--sub u-margin-bottom-small">
-                dominican touch in wesley chapel and lutz area
-              </span>
-            </h1>
-            <h2 className="heading-secondary">
-              Making Wesley Chapel and Lutz hair reflect its story and spirit.
-            </h2>
+          <h1 className="heading-primary">
+            <span className="heading-primary--main">hair stylist</span>
+            <span className="heading-primary--sub u-margin-bottom-small">
+              dominican touch in wesley chapel and lutz area
+            </span>
+          </h1>
+          <h2 className="heading-secondary">
+            Making Wesley Chapel and Lutz hair reflect its story and spirit.
+          </h2>
 
-            <div className="hero__logo-img-box">
-              <Image fluid={logoImg} alt="Hair stylist Hair by B logo image" />
-            </div>
-            <AnchorLink
-              to="/#contact"
-              title="contact"
-              className="btn btn--primary u-margin-bottom-medium u-margin-top-small"
-              stripHash
-            />
+          <div className="hero__logo-img-box">
+            <Image fluid={logoImg} alt="Hair stylist Hair by B logo image" />
           </div>
+          <AnchorLink
+            to="/#contact"
+            title="contact"
+            className="btn btn--primary u-margin-bottom-medium u-margin-top-small"
+            stripHash
+          />
+
           <div className="hero__hero-img-box">
             <Image fluid={heroImg} alt="Hair stylist Hair by B hero image" />
           </div>
